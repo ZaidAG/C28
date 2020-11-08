7a086b79af7616a90f2bbe0afb47f12c65cbd403
Zaid <gamerbrosimza@gmail.com>
2020-11-08 10:35:14 -0800

    KKKKkk constraint

--- sketch.js
+++ sketch.js
@@ -37,11 +37,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    
+    slingbot = new Slingshot1(bird.body,{x:200,y:100});
 }
-
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
@@ -63,6 +61,11 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    slingbot.display();    
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+    slingbot.fly();
 }
